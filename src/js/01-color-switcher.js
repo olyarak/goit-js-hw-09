5aec59543d68882ac68254a63e0ce91c50b39b40
Olha Rakytianska <olya.rakytyanska@gmail.com>
2023-02-11 00:29:57 +0200

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  timerId = setInterval(changeBodyColor, 1000);
+  startBtn.disabled = true;
+}
+function onStopClick() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+}
+function changeBodyColor() {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
